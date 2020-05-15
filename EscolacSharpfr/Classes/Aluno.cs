2d9c1168fdbd0b9b5339c9d48a613773fb615fe0
francisco8628 <francisco.ableite@gmail.com>
2020-05-14 22:02:21 -0300

    inserir classe aluno e executavel

---
+++ EscolacSharpfr/Classes/Aluno.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EscolacSharpfr.Classes
+{
+    class Aluno
+    {
+        private string nome;
+
+        private int idade;
+
+        private string Materia;
+
+		public String getNome()
+		{
+			return nome;
+		}
+
+		public void setNome(String nome)
+		{
+			this.nome = nome;
+		}
+
+		public int getIdade()
+		{
+			return idade;
+		}
+
+		public void setIdade(int idade)
+		{
+			this.idade = idade;
+		}
+
+		public String getMateria()
+		{
+			return Materia;
+		}
+
+		public void setMateria(String materia)
+		{
+			Materia = materia;
+		}
+
+
+
+	}
+}
---
+++ EscolacSharpfr/Executavel/ClasseExecutavel.cs
@@ -0,0 +1,23 @@
+﻿using EscolacSharpfr.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EscolacSharpfr.Executavel
+{
+    class ClasseExecutavel
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Aluno aluno = new Aluno();
+
+                aluno.setNome("francisco");
+
+                Console.WriteLine(aluno.getNome());
+            }
+        }
+
+    }
+}
